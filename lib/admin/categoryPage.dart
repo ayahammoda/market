133c3242101bd31b9fa4e66c39b17a3c00afb23b
leensaeed <leen2003tk@hotmail.com>
2023-06-05 23:32:58 +0300

    show category

--- lib/admin/categoryPage.dart
+++ lib/admin/categoryPage.dart
@@ -1,5 +1,9 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:market1/admin/categorychoice.dart';
+import 'package:market1/color.dart';
+import 'package:market1/color.dart';
 import 'package:market1/color.dart';
 import 'package:market1/constant.dart';
 import 'package:market1/models/category.dart';
@@ -16,10 +20,9 @@ class CategoryPage extends StatefulWidget {
 }
 
 class _CategoryPageState extends State<CategoryPage> {
-  final _store = Store();
-  List<Categories> ca = [];
 
-  int _tabBarIndex = 0;
+  CollectionReference usersRef = FirebaseFirestore.instance.collection(kCategoryCollection);
+
 
   @override
   Widget build(BuildContext context) {
@@ -30,109 +33,91 @@ class _CategoryPageState extends State<CategoryPage> {
     double fontSizeUnSelected = siz.width * .05;
 
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.pushNamed(context, CategoryCH.id);
-        },
-        tooltip: 'Increment',
-        backgroundColor: Colors.green,
-        child: const Icon(Icons.add),
-      ));
+    body:StreamBuilder<QuerySnapshot>(
+      stream: usersRef.snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        }
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return CircularProgressIndicator();
+        }
 
-    /*    //TODO here to view category
-        body:StreamBuilder<QuerySnapshot>(
-        stream:_store.loaCategory() ,
-        builder: (context, snapshot){
-          if (snapshot!=null) {
-             List<Categories> cat=[];
-            for (var doc in snapshot.data!.docs)
-            {
-             var data=doc.data;
-              cat.add( Categories(cateName:data[]),
-                  );
+        return GridView(
+          //crossAxisCount: 2,
 
-            }
-            return  GridView.builder(
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                childAspectRatio: .9,
-              ),
-              itemCount: cat.length,
-              itemBuilder: (context, index) => Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: GestureDetector(
-                  onTap: () {
-                    // Navigator.pushNamed(
-                    //   context,
-                    //   // ProductInfo.id,
-                    //   // arguments: _products[index],
-                    // );
-                  },
-                  child: Stack(
-                    children: [
-                      Positioned.fill(
-                        child: Image.asset(
-                          "Assets/Images/im1.jpg",
-                          fit: BoxFit.fill,
-                        ),
-                      ),
-                      Positioned(
-                        bottom: 0,
-                        child: Container(
-                          width: siz.width,
-                          height: siz.height * .29,
-                          color: KText.withOpacity(.1),
-                          child: Padding(
-                            padding: const EdgeInsets.all(0.0),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  'name product',
-                                  style: TextStyle(
-                                    color: KText,
-                                    fontSize: siz.width * .03,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                                Text(
-                                  'Price',
-                                  style: TextStyle(
-                                    color: KText,
-                                    fontSize: siz.width * .03,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                          // color: mainText.withOpacity(),
-                        ),
-                      ),
-                    ],
+          gridDelegate:SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent:200,
+          childAspectRatio: 3/2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,)
+
+          ,
+          children: snapshot.data!.docs.map((DocumentSnapshot document) {
+            Map<String, dynamic> data = document.data() as Map<String, dynamic>;
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  Container(
+                    width: siz.width,
+                    height: siz.height*.08,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(colors: [startc,endc],
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,),
+                          borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: GridTile(
+                      child: Center(child: Text(data[kCategoryName]
+                        ,style:TextStyle(fontSize:siz.height*.025) ,)),
+
+                    ),
                   ),
-                ),
+                  Row(
+                    children: [
+                        GestureDetector(
+                        onTap: (){},
+                        child: SizedBox(
+                        width: siz.width * .02,
+                        height: siz.height * .03,
+                        child: const Icon(Icons.delete),
+                         ),
+
+                        ),
+
+                      GestureDetector(
+                        onTap: (){},
+                        child: SizedBox(
+                        width: siz.width * .02,
+                        height: siz.height * .03,
+                        child: const Icon(Icons.edit),
+                         ),
+                        ),
+                ],
               ),
-            );
-              }
-          else {
-            return Text("Loading..");
-          }
-        },
-      )
-    ,
+            ],));
+          }).toList(),
+        );
+      },
+    ),
+
+
+
+
+        //TODO here to view category
+
         floatingActionButton: FloatingActionButton(
           onPressed: () {
             Navigator.pushNamed(context, CategoryCH.id);
           },
           tooltip: 'Increment',
-          backgroundColor: Colors.green,
+          backgroundColor: endc,
           child: const Icon(Icons.add),
-        ));
-  }*/
-
+    ),);
   }
+
+
 }
 Widget textTab({
   required String text,
--- lib/services/store.dart
+++ lib/services/store.dart
@@ -1,19 +1,7 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:market1/models/category.dart';
 
-import '../constant.dart';
 
-class Store {
-  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-  addcategory(Categories category) {
-    _firestore.collection(kCategoryCollection).add({
-      kCategoryName: category.cateName,
-    });
-  }
-  Stream<QuerySnapshot> loaCategory() {
-    return _firestore.collection(kCategoryCollection).snapshots();
-  }
-}
+
+
 
 
 
